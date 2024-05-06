c069699a76748c95d540edfdff75b1a8b72d60a0
jpauljayme <jpaul.jayme@gmail.com>
2024-05-06 10:27:36 +0800

    Activating prod env

--- src/main/java/dev/mayhm/blog/PersonalBlogApplication.java
+++ src/main/java/dev/mayhm/blog/PersonalBlogApplication.java
@@ -1,23 +1,13 @@
 package dev.mayhm.blog;
 
-import dev.mayhm.blog.mapper.PostMapper;
-import dev.mayhm.blog.model.Post;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class PersonalBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalBlogApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner commandLineRunner(PostMapper postMapper){
-		return args -> postMapper.insertPost(new Post("title", "author", "Test body"));
 	}
 }
